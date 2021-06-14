af137c438ee3bad295b8912bc3d8518e505336ad
sreeharshaparuchur1 <>
2021-06-15 00:13:55 +0530

    54

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 # An application blueprint - defines a bunch of URLs.
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>login</p>"
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
@@ -13,4 +13,4 @@ def logout():
 
 @auth.route('/sign_up')
 def sign_up():
-    return "<p>sign up</p>"
+    return render_template('sign_up.html')
